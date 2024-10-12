690d380a786becd5d36354fd541838a178e51eef
foziljon20 <uzfaraonyt@gmail.com>
2024-10-12 16:29:49 +0500

    Yangi fayllar qo'shildi

---
+++ python_7.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 24 16:18:01 2024
+
+@author: Diyor
+"""
+
+#sonlar = list(range(1,11))
+#for son in sonlar:
+#    print(f"{sonlar} ning kvadrati {sonlar**2} ga teng )
+
+          
+
+          
+
+          
+
+          
+
+#sonlar = list(range(1,11))
+#for son in sonlar:
+#    print(f"{son} ning kvadrati {son**2} ga teng")
+ 
+#sonlar = list(range(11))
+#sonlar_kvadrati = []
+#for son in sonlar:
+#     sonlar_kvadrati.append(son**2)
+
+
+
+dostlar = []
+print("5 ta eng yaqin do'stingiz kim?")
+for n in range(5):
+    dostlar.append(input(f"{n}-do'stingizni ismini kiriting:"))
+print(dostlar) 
