a18ef9ef5efbd5a89803174a5bbf4add01b0641d
foziljon20 <uzfaraonyt@gmail.com>
2024-10-12 17:51:36 +0500

    Yangi dars va yangi kodlar

---
+++ dars_14.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 12 16:43:00 2024
+
+12/10/2024
+
+Dasturlash asoslari
+
+#14-dars: Dictonary (Lug'at)
+
+Muallif: Foziljon Niyatullayev
+
+@author: Diyor
+"""
+
+#car_0 = {'model':'ferrari','rang':'qizil'}
+#print(car_0['model'])
+#print(car_0['rang'])
+
+#fi_0 = {'ism':'Foziljon','familya':'Niyatullayev'}
+#print(fi_0['ism'])
+#print(fi_0['familya'])
+
+en_uz = {'apple':'olma','apricot':"o'rik",'banana':"banan"}
+print()
+
+#mevalar = {'olma':10000,'tarvuz':8000,'qovun':7000}
+#print(f"Olma narxi {mevalar['olma']} so'm")
+
+#talaba_0 = {'ism':'foziljon niyatullayev','yosh':17,'t_yil':2007}
+#print(f"{talaba_0['ism'].title()},\
+#      {talaba_0['t_yil']}-yilda tug'ilgan,\
+#      {talaba_0['yosh']} yoshda.")
+      
+#talaba_0['kurs'] = 4 
+#talaba_0['fakultet'] = 'informatika'
+#talaba_0['ism']= 'ali'
+ 
+
+#talaba_1 = {}   
+#talaba_1['ism'] = 'qobil rasulov'
+#talaba_1['kurs'] = 4
+#talaba_1['yosh'] = 20
+#print(talaba_1)
+#print(f"Talaba {talaba_1['ism'].title()} {talaba_1['kurs']}-kurs va {talaba_1['yosh']} yoshda")
+
+#talaba_0 = {'ism':'foziljon niyatullayev','yosh':17,'t_yil':2007}
+# print{talaba_0}
+#del talaba_0['yosh']
+#print(talaba_0)
+
+telefonlar = {
+    'ali':'iphone x',
+    'vali':'galaxy s3',
+    'olim':'redmi 13 pro',
+    'orif':'nokia 3310'
+    }
+
+#phone = telefonlar['ali']
+#print(f"Alining telefoni {phone}")
+
+# # phone = relefonlar['hasa']
+# # print(f"Hasanning telefoni {phone}")
+
+meva = en_uz.get('pineapple','Bunday meva mavjud emas')
+#print(meva)
+
+#phone = telefonlar.get('hasan')
+#print(phone)
+
+
+
+
